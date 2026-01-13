db1e3cf1465383bb0004be1a3f3b9a2e0d53081c
Paulo <paulosantosjrrda!gmail.com>
2026-01-13 17:40:47 -0300

    estilizando a sidebar

--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Title from "../../components/Title"
+import { Descricao, BotaoTema, SideBarContainer } from "./styles"
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/PauloJnnr.png" />
-    <Title fontSize={20}>PauloJnnr</Title>
+    <SideBarContainer>
+      <Avatar />
+      <Title fontSize={20}>Paulo Júnior</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        PauloJnnr
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Fullstack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
